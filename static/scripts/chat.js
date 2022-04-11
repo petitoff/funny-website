e14c799ffce58d1fb7d0e29a196a345cce6eaf51
Błażej Domagała <petitoff@outlook.com>
2022-04-11 20:30:12 +0200

    Adding a function
    Moving the function inside the event method to a separate function so that it can be called with the enter key and the button.

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,7 +1,10 @@
 const getMessageFromUser = document.getElementById("inputMessage");
-getMessageFromUser.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
+const btn1 = document.querySelector(".button");
+
+const event1 = function (event) {
+  if (event.key === "Enter" || event == "[object PointerEvent]") {
     const userMessage = document.getElementById("inputMessage").value; // Retrieving messages from the user
+    allMessageFromUser.push(userMessage);
     document.getElementById("inputMessage").value = ""; // Clearing the input from the user
 
     document
@@ -10,5 +13,10 @@ getMessageFromUser.addEventListener("keyup", function (event) {
         "beforeend",
         `<div class="chat outgoing"><div class="details"><p> ${userMessage} </p></div></div>`
       );
+
+    setTimeout(responseFromBot, 1000);
   }
-});
+};
+
+getMessageFromUser.addEventListener("keyup", event1);
+btn1.addEventListener("click", event1);
