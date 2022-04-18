c5595832414e9972d4188e78437a362fc7bfe670
Błażej Domagała <petitoff@outlook.com>
2022-04-18 14:52:21 +0200

    Transferring the code to a separate function

--- static/scripts/welcomeMessage.js
+++ static/scripts/welcomeMessage.js
@@ -2,17 +2,6 @@ function responseFromBotW() {
   const botMessageToUser =
     pattern.welcomeMessage ?? errorMessage.errorFindingTheMessage;
 
-  document
-    .getElementById("chatbot_sender")
-    .insertAdjacentHTML(
-      "beforeend",
-      `<div class="animation-div-mov chat incoming"><div class="details"><p> ${botMessageToUser} </p></div></div>`
-    );
-  scrollDownChat();
+  animationDivMovForBotMessage(botMessageToUser);
 }
-
-// setTimeout(responseFromBotW, 1000);
 responseFromBotW();
-const animation1 = document.querySelector(".animation-div-mov");
-
-animation1.classList.add("move");
